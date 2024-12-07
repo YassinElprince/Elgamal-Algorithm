47afb4b5f4a74849012fe10615ad69cd1b51a182
YassinElprince <yassin.elprince2005@gmail.com>
2024-12-07 02:51:16 +0200

    Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -1,1 +1,6 @@
 
+p = int(input("Enter your prime number: "))
+g = int(input("Enter your primitive root: "))
+h = int(input("Enter your public key: "))
+public_key = (p, g, h)
+message = input("Enter your message: ")
